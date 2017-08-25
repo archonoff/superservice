c085e37a090c5cfaed7a82c53b8a8480426586ce
archonoff <archonoff@gmail.com>
2017-08-25 10:48:23 +0300

    начало aiohttp сервера

--- superserver_async.py
+++ superserver_async.py
@@ -1,2 +1,19 @@
-import aiohttp
-from .superservice import fulfill_order, post_order, get_open_orders, get_users, create_user
+from aiohttp import web
+from superservice import fulfill_order, post_order, get_open_orders, get_users, create_user     # fixme функции запросов к базе синхронные!
+
+
+async def index(request):
+    return web.HTTPTemporaryRedirect('/orders/')
+
+
+async def orders_list(request):
+    open_orders = get_open_orders()
+    return web.json_response(open_orders)
+
+
+app = web.Application()
+app.router.add_get('/', index)
+app.router.add_get('/orders/', orders_list)
+# app.router.add_get('/{name}', handle)
+
+web.run_app(app, host='localhost', port=8000)
