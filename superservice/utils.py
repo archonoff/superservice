4dd53828aa1bab5a27f6d40613754d763458927d
oleg-mac <o.zaytsev@greenlabs.ru>
2017-08-26 22:15:34 +0300

    closures for rest response

--- superservice/utils.py
+++ superservice/utils.py
@@ -3,17 +3,14 @@ import ujson
 from aiohttp.web import json_response, Response
 
 
-def rest_response(code: str, message) -> Response:
-    result_dict = {
-        'result': code,
-        'message': message,
-    }
-    return json_response(result_dict, dumps=ujson.dumps)
+def code_response(code: str) -> Response:
+    def rest_response(data=None):
+        result_dict = {
+            'result': code,
+            'data': data,
+        }
+        return json_response(result_dict, dumps=ujson.dumps)
+    return rest_response
 
-
-def success_response(message=None):
-    return rest_response('success', message)
-
-
-def error_response(message=None):
-    return rest_response('error', message)
+success_response = code_response('success')
+error_response = code_response('error')
